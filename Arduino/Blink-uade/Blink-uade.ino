a910d56990543ecd29f453b44dc64c6d1e4a3bb3
Jao Ramos <64828457+JaoRamos@users.noreply.github.com>
2025-08-13 17:34:41 -0300

    Update Blink-uade.ino

--- Arduino/Blink-uade/Blink-uade.ino
+++ Arduino/Blink-uade/Blink-uade.ino
@@ -1,13 +1,13 @@
 // La función setup se ejecuta una sola vez cuando presionas reset o encendes la placa
 void setup() {
   // Inicializa el pin digital LED_BUILTIN como salida
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 // La función loop se ejecuta repetidamente en un ciclo infinito
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // Encender el LED (HIGH es el nivel de tensión ALTO o 5V)
+  digitalWrite(13, HIGH);  // Encender el LED (HIGH es el nivel de tensión ALTO o 5V)
   delay(1000);                      // espera por 1 segundo (1000ms)
-  digitalWrite(LED_BUILTIN, LOW);   // Apagar el LED poniendo la tensión en LOW (0V)
+  digitalWrite(13, LOW);   // Apagar el LED poniendo la tensión en LOW (0V)
   delay(1000);                      // espera por 1 segundo (1000ms)
 }
